08b8ed8c36bd482809642fe1a5f537f293cbd4ad
SamDingore <sameerdingore.iam@gmail.com>
2025-02-05 16:09:42 -0800

    Initialize project structure with configuration files and basic services

---
+++ frontend/app/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { Welcome } from '../components/Welcome/Welcome';
+
+export default function HomePage() {
+  return (
+    <>
+      <Welcome />
+      <ColorSchemeToggle />
+    </>
+  );
+}
