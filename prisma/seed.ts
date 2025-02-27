1d6d8244633388f049ef21f0c04cb8f4d170617b
SamDingore <sameerdingore.iam@gmail.com>
2025-02-27 14:55:30 -0800

    Add privilege seeding logic to the database

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -30,11 +30,43 @@ async function main() {
     },
   ];
 
+  const privileges = [
+    {
+      uuid: "e2f4687a-1fbe-4871-89d0-510bd6c5b7c4",
+      action: "#1100,",
+      resource: "#role,",
+      status: RoleStatus.ACTIVE,
+      date_created: new Date(),
+      date_modified: new Date(),
+      role_uuid: "87ca7b7a-700e-459c-82f2-1381c6fe090c",
+    },
+    {
+      uuid: "beddd438-f05b-4d8d-a5b5-8f03c81c9fff",
+      action: "#1100,",
+      resource: "#role,",
+      status: RoleStatus.ACTIVE,
+      date_created: new Date(),
+      date_modified: new Date(),
+      role_uuid: "ae818b0a-5ff2-454d-be4a-1f7cfa4ed7bb",
+    },
+    
+
+  ]
+  
+
   for (const role of roles) {
     await prisma.role.upsert({
       where: { uuid: role.uuid },
       update: role,
       create: role,
+    });
+  }
+
+  for (const privilege of privileges) {
+    await prisma.privilege.upsert({
+      where: { uuid: privilege.uuid },
+      update: privilege,
+      create: privilege,
     });
   }
 
