8a6b3421576065bb36d132e5683330b8e129928a
Shaun Rose <thefrznrose@gmail.com>
2025-03-05 17:01:10 -0800

    Added my frontend instead of re-writing it from scratch

---
+++ frontend/app/student/taking-exam/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import TakingExam from "@/components/Student/Exam/TakingExam/TakingExam";
+
+export default function takingExam () {
+    return (
+        <>
+            <TakingExam />
+        </>
+    );
+}
