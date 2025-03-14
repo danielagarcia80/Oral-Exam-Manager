567225193703d8f7cb436f2579903ebad13652ee
SamDingore <sameerdingore.iam@gmail.com>
2025-03-14 11:45:02 -0700

    Refactor ModuleCard and DashBoard components; integrate mock data and enhance styling

--- frontend/components/DashBoard/DashBaord.tsx
+++ frontend/components/DashBoard/DashBaord.tsx
@@ -1,9 +1,22 @@
 import ModuleCard from "../ModuleCard/ModuleCard";
 
+
+const mockdata = {
+    image:
+        'https://images.unsplash.com/photo-1518770660439-4636190af475?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Course 0',
+    badge: 'Fall 2025',
+    description: "XXXX - XXXX"
+};
 export default function DashBoard() {
     return (
         <div>
-            <ModuleCard />
+            <ModuleCard
+                image={mockdata.image}
+                title={mockdata.title}
+                badge={mockdata.badge}
+                description={mockdata.description}
+            />
         </div>
     );
 }
--- frontend/components/ModuleCard/ModuleCard.tsx
+++ frontend/components/ModuleCard/ModuleCard.tsx
@@ -2,17 +2,11 @@ import { IconHeart } from '@tabler/icons-react';
 import { ActionIcon, Badge, Button, Card, Group, Image, Text } from '@mantine/core';
 import classes from './ModuleCard.module.css';
 
-const mockdata = {
-    image:
-        'https://images.unsplash.com/photo-1437719417032-8595fd9e9dc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=80',
-    title: 'Course 0',
-    country: 'Fall 2025',
-    description:"XXXX - XXXX"
-};
 
-export default function ModuleCard() {
-    const { image, title, description, country } = mockdata;
 
+export default function ModuleCard(
+    { image, title, description, badge }: { image: string, title: string, description: string, badge: string}
+) {
     return (
         <Card withBorder w={300} radius="md" p="md" className={classes.card}>
             <Card.Section>
@@ -25,7 +19,7 @@ export default function ModuleCard() {
                         {title}
                     </Text>
                     <Badge size="sm" variant="light">
-                        {country}
+                        {badge}
                     </Badge>
                 </Group>
                 <Text fz="sm" mt="xs">
